f985f4a5e0481922bb8c3c0cfc3402bb7f8b54b4
ted bracht <ted@bracht.uk>
2020-11-20 20:20:49 +0000

    Build 20.4.0

--- build_exe.py
+++ build_exe.py
@@ -2,7 +2,7 @@ import PyInstaller.__main__
 import os.path
 PyInstaller.__main__.run([
     '--name=%s' % "rp_collector",
-    '--distpath=%s' % "dist/rp_collector_v20.3.2",
+    '--distpath=%s' % "dist/rp_collector_v20.4.0",
     '--onefile',
 #    '--add-binary=%s' % './drivers/chromedriver.exe',
 #    '--manifest=%s' % 'wrac_classes.exe.manifest',
