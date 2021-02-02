4ffac16556c391e4882e25a610b6ddc08a5fa3f8
ted bracht <ted@bracht.uk>
2021-02-02 06:38:40 +0000

    Built version 21.0.0

--- build_exe.py
+++ build_exe.py
@@ -2,10 +2,10 @@ import PyInstaller.__main__
 import os.path
 PyInstaller.__main__.run([
     '--name=%s' % "rp_collector",
-    '--distpath=%s' % "dist/rp_collector_v20.4.0",
+    '--distpath=%s' % "dist/rp_collector_v21.0.0",
     '--onefile',
 #    '--add-binary=%s' % './drivers/chromedriver.exe',
 #    '--manifest=%s' % 'wrac_classes.exe.manifest',
-    '--icon=%s' % os.path.join('.', 'resources', 'equestrian.ico'),
+    '--icon=%s' % os.path.join('.', 'resources', 'race-horse.ico'),
     'main.py'
 ])
