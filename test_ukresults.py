c0cd2abf07ba1c5dbd020871017ede9bcfbff19e
ted bracht <ted@bracht.uk>
2022-01-16 12:59:20 +0000

    start to parse race lines

--- test_ukresults.py
+++ test_ukresults.py
@@ -16,26 +16,52 @@ get_races scenarios
 4. we're in Jan and want Dec last year and Jan this year
     get Dec from last year's index page
     get Jan from this year's index page
-
+5. we're in Apr and want the wole of last year, July - Dec of the year before and the first 3 months of this year
+    get the the last 6 months of the year before last
+    get the whole of last year
+    get the first 3 months of this year
 https://www.ukresults.net/2021/index.html
 https://www.ukresults.net/2022/index.html
 """
 
 
 
+def test_parse_race_line():
+    line = '<tr><td>05 April</td><td><a href="fast5kapra.html"><b>Fast 5k Races, Three Sisters Circuit, Wigan, Lancashire</b></a></td></tr>'
+    bs_line = bs(line, "html.parser")
+    my_race = ukr.parse_race_row(bs_line, 4, 2021, "/base_url/")
+    assert my_race.date == date.fromisoformat("2021-04-05")
+    assert my_race.url == "/base_url/fast5kapra.html"
+    assert my_race.event == "Fast 5k Races, Three Sisters Circuit, Wigan, Lancashire"
+
+
 @pytest.fixture()
-def index_page():
+def index_page_2021():
     with open('test-pages/ukresults/index_2021.html', "r") as file:
         return file.read()
 
+@pytest.fixture()
+def index_page_2022():
+    with open('test-pages/ukresults/index_2022.html', "r") as file:
+        return file.read()
 
-def test_get_races(index_page):
-    races = ukr.get_races(index_page, date.fromisoformat('2021-01-01'))
+def test_get_races(index_page_2021):
+    races = ukr.get_races(index_page, year, date.fromisoformat('2021-01-01'), base_url)
     assert len(races) == 109  # add assertion here
 
-def test_get_selection_of_races(index_page):
-    races = ukr.get_races(page, date.fromisoformat('2021-10-01'))
+def test_get_selection_of_races(index_page_2021):
+    races = ukr.get_races(index_page, year, date.fromisoformat('2021-10-01'), base_url)
     assert len(races) == 34  # add assertion here
+
+def test_get_index_pages():
+    page_urls = ukr.get_index_page_urls('base_url',date.fromisoformat('2021-11-01'),date.fromisoformat('2021-12-31'))
+    assert len(page_urls) == 1
+    page_urls = ukr.get_index_page_urls('base_url',date.fromisoformat('2021-01-01'),date.fromisoformat('2021-12-31'))
+    assert len(page_urls) == 1
+    page_urls = ukr.get_index_page_urls('base_url',date.fromisoformat('2021-12-01'),date.fromisoformat('2022-01-16'))
+    assert len(page_urls) == 2
+    page_urls = ukr.get_index_page_urls('base_url',date.fromisoformat('2020-07-01'),date.fromisoformat('2022-03-31'))
+    assert len(page_urls) == 3
 
 
 def test_year_urls():
--- ukresults.py
+++ ukresults.py
@@ -12,20 +12,60 @@ def get_results(line, base_url, test):
     results = []
     return results
 
+def parse_race_row(row, month, year, base_url):
+    '"<tr><td>05 April</td><td><a href="fast5kapra.html"><b>Fast 5k Races, Three Sisters Circuit, Wigan, Lancashire</b></a></td></tr>"'
+    try:
+        date_string = row.find("td").text
+        a = row.find("a")
+        link = a['href']
+        name = a.text
+        #create date
+        day = date_string.split(" ")[0]
+        race_date = date.fromisoformat(f"{year}-{month:02d}-01")
+        race = result.Race()
+        race.date = race_date
+        race.event = name
+        race.url = f"{base_url}/{year}/{link}"
+        return race
+    except:
+        return None
 
-def get_races(page, from_date):
+def get_races(page, year, from_date, to_date, base_url):
     races = []
+    bs = BeautifulSoup(page, "html.parser")
+    months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+    for idx, month in enumerate(months):
+        try:
+            bs_month = bs.find("a", {"name": month})
+            bs_table = bs.find("table", {"class": "results"})
+            rows = bs_table.findAll("tr")
+            for row in rows:
+                race = parse_race_row(row, idx+1, year, base_url)
+                if race:
+                    races.append(race)
+        except:
+            None
     return races
+
+
+def get_index_page_urls(base_url, from_date, to_date):
+    urls = []
+    for year in range(from_date.year, to_date.year + 1):  # range excludes last number
+        urls.append((year, f"{base_url}/{year}/index.html"))
+    return urls
 
 
 def collect_result(base_url, weeks, test):
     results = []
-    logger.info("Retrieving the index page ...")
-    from_date = scr_utils.find_from_date(weeks, date.today())
-    for year in range(from_date.year,date.today().year + 1): # range excludes last number
-        page = scr_utils.getpage(f"{base_url}/{year}/index.html", "ukresults index")
+    logger.info("Retrieving the index page(s) ...")
+    to_date = date.today()
+    from_date = scr_utils.find_from_date(weeks, to_date)
+    index_page_urls = get_index_page_urls(from_date, to_date)
+    for url in index_page_urls:
+        year = url[0]
+        page = scr_utils.getpage(url[1], f"ukresults index {year}")
         if page:
-            races = get_races(page, from_date)
+            races = get_races(page, year, from_date, to_date, base_url)
             logger.info(f"Retrieving the individual race pages; {len(races)} in total")
             for race in races:
                 my_result = get_results(race, base_url, test)
