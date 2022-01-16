7409757da5c351edafe47e0285a3399741776ffa
ted bracht <ted@bracht.uk>
2022-01-16 07:52:03 +0000

    overlapping years index pages at ukresults

--- test_ukresults.py
+++ test_ukresults.py
@@ -5,10 +5,27 @@ from datetime import date
 import ukresults as ukr
 import result
 
+"""
+get_races scenarios
+1. we're in Dec and want Nov and Dec
+    just one index page
+2. we're in Dec and want Jan - Dec
+    just one index page
+3. we're in Jan and want the whole of last year
+    get last year's index page
+4. we're in Jan and want Dec last year and Jan this year
+    get Dec from last year's index page
+    get Jan from this year's index page
+
+https://www.ukresults.net/2021/index.html
+https://www.ukresults.net/2022/index.html
+"""
+
+
 
 @pytest.fixture()
 def index_page():
-    with open('test-pages/ukresults/index.html', "r") as file:
+    with open('test-pages/ukresults/index_2021.html', "r") as file:
         return file.read()
 
 
