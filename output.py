e86f96b739464c61a42b299f7bd385bbf9eaa00d
ted bracht <ted@bracht.uk>
2022-02-06 17:14:29 +0000

    pdf output and html output - not tested

--- output.py
+++ output.py
@@ -1,19 +1,51 @@
 import csv
 import os
+import fpdf
+from datetime import date
 
 import result
 import config
 
+fpdf.set_global("SYSTEM_TTFONTS", os.path.join(os.path.dirname(__file__),'fonts'))
+
 output_path = config.output_dir()
+today = date.today().strftime("%y-%m-%d")
+basename = f"{output_path}/results-{today}"
 
 if not os.path.exists(output_path):
     os.makedirs(output_path)
 
 def output_as_csv(results):
-    with open(output_path + '/' + 'output.csv', mode='w', newline='') as csv_file:
+    filename = basename + '.csv'
+    with open(filename, mode='w', newline='') as csv_file:
         fieldnames = result.Result.fields()
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
         writer.writeheader()
         for line in results:
             writer.writerow(line.to_dict())
+        return filename
+
+def output_as_pdf(results):
+    filename = basename + '.pdf'
+    pdf = fpdf.FPDF()
+    pdf.add_font("NotoSans", style="", fname="NotoSans-Regular.ttf", uni=True)
+    pdf.add_font("NotoSans", style="B", fname="NotoSans-Bold.ttf", uni=True)
+    pdf.add_font("NotoSans", style="I", fname="NotoSans-Italic.ttf", uni=True)
+    pdf.add_font("NotoSans", style="BI", fname="NotoSans-BoldItalic.ttf", uni=True)
+    pdf.add_page()
+    pdf.set_font('NotoSans', 'B', 16)
+    for race in results:
+        pdf.cell(0, 10, f"{race.date} - {race.event} - Winning time: {race.winningtime}", 0, 1)
+        pdf.set_font('NotoSans', '', 12)
+        for runner in race.runners:
+            pdf.cell(0, 7, runner.textoutput, 0, 1)
+    pdf.output(filename, 'F')
+    return filename
+
+def output_as_html_table(results):
+    filename = basename + '.txt'
+    with open(filename, mode='w', newline='') as text_file:
+        for race in results:
+            text_file.write(race.html_race_output())
+    return filename
--- result.py
+++ result.py
@@ -36,6 +36,29 @@ class Result:
     def to_dict(self):
         return asdict(self)
 
+    def duathlon(self):
+        return True if self.run1 and self.bike else False
+
+    def triathlon(self):
+        return True if self.swim and self.bike else False
+
+    def textoutput(self):
+        if self.duathlon():
+            return f"pos: {self.pos} name: {self.name} cat: {self.category} run1: {self.run1} t1: {t1} bike: {self.bike} t2: {self.t2} run2: {self.run2} total: {self.time}"
+        elif self.triathlon():
+            return f"pos: {self.pos} name: {self.name} cat: {self.category} swim: {self.swim} t1: {t1} bike: {self.bike} t2: {self.t2} run: {self.run} total: {self.time}"
+        else:
+            return f"pos: {self.pos} name: {self.name} cat: {self.category} time: {self.time}"
+
+    def tableoutput(self):
+        if self.duathlon():
+            return f"<tr><td>{self.pos}</td><td>{self.name}</td><td>{self.category}</td><td>{self.run1}</td><td>{t1}</td><td>{self.bike}</td><td>{self.t2}</td><td>{self.run2}</td><td>{self.time}</td></tr>"
+        elif self.triathlon():
+            return f"<tr><td>{self.pos}</td><td>{self.name}</td><td>{self.category}</td><td>{self.swim}</td><td>{t1}</td><td>{self.bike}</td><td>{self.t2}</td><td>{self.run}</td><td>{self.time}</td></tr>"
+        else:
+            return f"<tr><td>{self.pos}</td><td>{self.name}</td><td>{self.category}</td><td>{self.time}</td></tr>"
+
+
 @dataclass
 class Race():
     date: str = ''
@@ -47,3 +70,33 @@ class Race():
     winningtime: str = ''
     year: str = ''
     base_url: str = ''
+    runners: list[Result]
+
+    def duathlon(self):
+        return True if self.runners and self.runners[0].duathlon() else False
+
+    def triathlon(self):
+        return True if self.runners and self.runners[0].triathlon() else False
+
+    def tableheaderoutput(self):
+        tabledef = '<div class="table-1"><table width="100%"><thead>'
+        tabledefend = '</thead><tbody>'
+        if self.duathlon():
+            return f"{tabledef}<tr><th>pos</th><th>name</th><th>cat</th><th>run1</th><th>t1</th><th>bike</th><th>t2</th><th>run2</th><th>total</th></th>{tabledefend}"
+        elif self.triathlon():
+            return f"{tabledef}<tr><th>pos</th><th>name</th><th>cat</th><th>swim</th><th>t1</th><th>bike</th><th>t2</th><th>run</th><th>total</th></th>{tabledefend}"
+        else:
+            return f"{tabledef}<tr><th>pos</th><th>name</th><th>cat</th><th>time</th></tr>{tabledefend}"
+
+    def tableend(self):
+        return '</tbody></table></div>'
+
+    def html_race_output(self):
+        output = ""
+        if self.runners:
+            output = f"<h3><b>{self.date} - {self.event}</b></h3>{self.tableheaderoutput()}"
+            for runner in self.runners:
+                output += runner.tableoutput()
+            output += self.tableend()
+        return output
+
